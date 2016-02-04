287018b72e6c2dcc1982c29513d9b643b7f1ca9d
jwhitwo <jnwhitwo@uncg.edu>
2016-02-04 08:57:29 -0500

    Skeleton of program 2 classes
    This is the skeleton of the program 2 classes as well as the Game.class.

---
+++ MyGame.java
@@ -0,0 +1,17 @@
+/*
+ * Author: 
+ * Program 2 - MyGame
+ * CSC230-02 Spring 2016
+ */
+package mygame;
+
+public class MyGame {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
